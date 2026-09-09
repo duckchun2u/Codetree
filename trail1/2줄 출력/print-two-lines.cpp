b52f38221990854f2c1524590471f9ac1e7f5e76
duckchun <duckchun2u@gmail.com>
2026-09-09 11:17:34 +0900

    [Passed] 2줄 출력 (2ms, 0MB)

---
+++ trail1/2줄 출력/print-two-lines.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    // endl == endline
+    // \n == newline
+
+    cout << "Hello" << "\n" << "World";
+
+    return 0;
+}
