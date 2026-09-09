eb71eb97aef247f6922ced3f31024a5f84a4aa63
duckchun <duckchun2u@gmail.com>
2026-09-09 14:14:41 +0900

    [Passed] 세 정수형 변수 선언 (2ms, 0MB)

---
+++ trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 7, b = 23, c = a + b;
+    cout << "7 + 23 = " << c;
+    
+    return 0;
+}
