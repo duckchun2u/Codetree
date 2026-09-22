252af46268e875d97048c8260438b83075778796
duckchun <duckchun2u@gmail.com>
2026-09-22 14:57:13 +0900

    [Passed] 합과 평균의 차 (1ms, 0MB)

---
+++ trail1/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, sum;
+    cin >> a >> b >> c;
+    sum = a + b + c;
+    cout << sum << "\n";
+    cout << sum / 3 << "\n";
+    cout << sum - sum/3;
+
+    return 0;
+}
